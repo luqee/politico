967b97ca56da4e21b8f36a38276bcf8cad5977dc
Luke Nzang'u <saintlukke@gmail.com>
2019-02-11 21:42:55 +0300

    [Finishes #163841956] Ensure links are working.

---
+++ UI/static/js/adminLogin.js
@@ -0,0 +1,4 @@
+var adminLogin = function(e){
+    e.preventDefault();
+    window.location.href = '../admin/adminHome.html';
+}
--- UI/static/js/index.js
+++ UI/static/js/index.js
@@ -3,11 +3,12 @@ var modal = document.getElementById('deleteModal');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
+var userSignupButton = document.getElementById('userSignupButton');
+userSignupButton.onclick = userSignup;
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 var showModal = function(){
     modal.style.display = "block";
@@ -21,4 +22,10 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+var userSignup = function(e){
+  e.preventDefault();
+  window.location.href = 'userLogin.html';
+  console.log(window.location.href)
 }
