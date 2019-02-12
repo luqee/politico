e39d8041997318e5159daace559bbece76009057
Luke Nzang'u <saintlukke@gmail.com>
2019-02-12 04:20:09 +0300

    [Feature #163620392] Style the voting page info section.

--- UI/static/js/index.js
+++ UI/static/js/index.js
@@ -1,24 +1,41 @@
-// Get the modal
-var modal = document.getElementById('deleteModal');
+window.onload = function(){
+  // Get the modal
+  var modal = document.getElementById('deleteModal');
+  // User vote modal
+  var voteModal = document.getElementById('voteModal');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  // var voteCloseSpan = document.getElementsByClassName("closeVote")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  voteCloseSpan.onclick = function() {
+    voteModal.style.display = 'none';
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+  }
+  
+  
+}
+var showModal = function(){
+  modal.style.display = "block";
+}
+var hideModal = function(){
   modal.style.display = "none";
 }
 
-var showModal = function(){
-    modal.style.display = "block";
+// User vote
+var selectCandidate = function(){
+  voteModal.style.display = "block";
 }
-var hideModal = function(){
-    modal.style.display = "none";
+var hideVoteModal = function(){
+  voteModal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
